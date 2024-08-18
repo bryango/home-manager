759dca51724eeec48fea6165e15b31994d8663c4
Bryan Lai <bryanlais@gmail.com>
2024-08-18 10:49:33 +0800

    home-manager/default.nix: use `postFixup`

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -34,6 +34,20 @@ installPhase = ''
   install -v -D -m755 home-manager/home-manager $out/bin/home-manager
   install -v -D -m755 lib/bash/home-manager.sh $out/share/bash/home-manager.sh
 
+  installShellCompletion --bash --name home-manager.bash home-manager/completion.bash
+  installShellCompletion --fish --name home-manager.fish home-manager/completion.fish
+  installShellCompletion --zsh --name _home-manager home-manager/completion.zsh
+
+  for pofile in home-manager/po/*.po; do
+    lang="''${pofile##*/}"
+    lang="''${lang%%.*}"
+    mkdir -p "$out/share/locale/$lang/LC_MESSAGES"
+    msgfmt -o "$out/share/locale/$lang/LC_MESSAGES/home-manager.mo" "$pofile"
+  done
+
+  runHook postInstall
+'';
+postFixup = ''
   substituteInPlace $out/bin/home-manager \
     --subst-var-by bash "${bash}" \
     --subst-var-by DEP_PATH "${
@@ -52,18 +66,5 @@ installPhase = ''
     --subst-var-by HOME_MANAGER_LIB "$out/share/bash/home-manager.sh" \
     --subst-var-by HOME_MANAGER_PATH "${if path == pkgs.path /* `path` is not passed to `callPackage` */ then finalAttrs.src else pathStr}" \
     --subst-var-by OUT "$out"
-
-  installShellCompletion --bash --name home-manager.bash home-manager/completion.bash
-  installShellCompletion --fish --name home-manager.fish home-manager/completion.fish
-  installShellCompletion --zsh --name _home-manager home-manager/completion.zsh
-
-  for pofile in home-manager/po/*.po; do
-    lang="''${pofile##*/}"
-    lang="''${lang%%.*}"
-    mkdir -p "$out/share/locale/$lang/LC_MESSAGES"
-    msgfmt -o "$out/share/locale/$lang/LC_MESSAGES/home-manager.mo" "$pofile"
-  done
-
-  runHook postInstall
 '';
 })
